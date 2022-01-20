fb2d36faaccf4f8512dc139bb6d58719d16cd4ca
Matthew Castrigno <matthewcastrigno@u.boisestate.edu>
2022-01-20 15:30:24 -0700

    column not resizing

---
+++ RComm/src/Vertical_test.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import Split from "react-split";
+import Rooms from "./Rooms";
+import Staff from "./Staff";
+import Events from "./Events";
+import "./Split.css";
+
+function Vertical_test() {
+  return (
+    <>
+      <Split
+        sizes={[60, 40]}
+        minSize={200}
+        expandToMin={false}
+        gutterSize={10}
+        gutterAlign="center"
+        snapOffset={30}
+        dragInterval={1}
+        direction="vertical"
+        cursor="row-resize"
+        className="split-flex-column"
+      >
+        <Staff />
+        <Events />
+      </Split>
+    </>
+  );
+}
+export default Vertical_test;
--- RComm/src/main.jsx
+++ RComm/src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import RComm_layout from "./RComm_layout";
+import Vertical_test from "./Vertical_test";
 
 ReactDOM.render(
   <React.StrictMode>
     <RComm_layout />
-    <App />
+    <Vertical_test />
   </React.StrictMode>,
   document.getElementById("root")
 );

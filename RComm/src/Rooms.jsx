975b87baf347a90ea2e6b359087573275e5aaeb4
Matthew Castrigno <matthewcastrigno@u.boisestate.edu>
2022-01-24 11:06:45 -0700

    pre cleanup commit

--- RComm/src/Rooms.jsx
+++ RComm/src/Rooms.jsx
@@ -5,13 +5,13 @@ function Rooms() {
   return (
     <>
       <div className={"rooms"}>
-        <div>
+        <div className={"room_type"}>
           <div>Proc1</div>
           <div>Proc2</div>
           <div>Proc3</div>
           <div>Proc4</div>
         </div>
-        <div>
+        <div className={"room_type"}>
           <div>Clinic1</div>
           <div>Clinic2</div>
           <div>Clinic3</div>
